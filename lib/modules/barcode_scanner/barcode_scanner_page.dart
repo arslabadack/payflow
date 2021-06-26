cae4c0675e265c8a39114c04766d9e3e7275a0cc
Adam Slabadack <arslabadack@gmail.com>
2021-06-26 13:54:44 -0300

    State updated

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -21,7 +21,7 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
     controller.getAvailableCameras();
     controller.statusNotifier.addListener(() {
       if (controller.status.hasBarcode) {
-        Navigator.pushReplacementNamed(context, "/insert_boleto",
+        Navigator.pushNamed(context, "/insert_boleto",
             arguments: controller.status.barcode);
       }
     });
@@ -90,7 +90,7 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
                 bottomNavigationBar: SetLabelButtons(
                     primaryLabel: "Inserir código do boleto",
                     primaryOnPressed: () {
-                      Navigator.pushReplacementNamed(context, "/insert_boleto");
+                      Navigator.pushNamed(context, "/insert_boleto");
                     },
                     secondaryLabel: "Adicionar da galeria",
                     secondaryOnPressed: () {})),
@@ -109,7 +109,7 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
                   },
                   secondaryLabel: "Digitar código",
                   secondaryOnPressed: () {
-                    Navigator.pushReplacementNamed(context, "/insert_boleto");
+                    Navigator.pushNamed(context, "/insert_boleto");
                   },
                 );
               } else {
